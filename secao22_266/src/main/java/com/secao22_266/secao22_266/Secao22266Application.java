d3c58be40384d443a11fa296edadc16d438ee5bb
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2025-03-26 16:47:56 -0300

    workshop-springboot-jpa

---
+++ secao22_266/src/main/java/com/secao22_266/secao22_266/Secao22266Application.java
@@ -0,0 +1,15 @@
+package com.secao22_266.secao22_266;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication é uma anotação, para configurar este projeto, para que ele seja uma aplicação springboot.
+//O SpringBoot, faz bastante o uso das anotation, para configurar o nosso código.
+@SpringBootApplication
+public class Secao22266Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao22266Application.class, args);
+	}
+
+}
